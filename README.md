9ada8a012d107bd72fcf94d11c7379ba069c2952
Wesley Reardan <wesley@4kdev.com>
2018-07-28 14:13:08 -0700

    Add basic app skeleton

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 This is the code repository for Healthie.us
-The main application is broken down into four screens:
-Registration
-Login
-Records
-Communication
+
+healthie.us
+	Health Records App
+
+	Integrations:
+		Trulioo - Patient and Doctor verification 
+		FitBit - Patient data
+		Agora - Emergency communication
+
+	Screens
+		Registration screen
+			Account creation
+			Trulio verification of patient
+
+		Login screen
+			Trulio verification of doctor
+
+		Records screen
+
+		Doctor Communication
+			
---
+++ index.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import request
+import flask
+import time
+from datetime import datetime
+import json
+import hashlib
+from flask import Response, stream_with_context, redirect, flash, render_template, session, abort
+import boto3, botocore
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', defaults={'path': ''}, methods = ['PUT', 'GET'])
+@app.route('/<path:path>', methods = ['PUT', 'GET'])
+def home(path):
+    if not 'user_id' in session or not (int(session['user_id']) > 0):
+        return redirect('/login')
+    return render_template('home.html')
+
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        user_id = db.get('Username:' + username)
+
+        if user_id == '' or user_id is None:
+            return render_template('login.html')
+        if password_hash != db_password_hash:
+            return render_template('login.html')
+
+        session['user_id'] = user_id
+
+        return redirect('/')
+
+
+@app.route('/records', methods = ['GET', 'POST'])
+def records():
+    return render_template('records.html')
+
+
+@app.route('/communicate', methods = ['GET', 'POST'])
+def communicate():
+    return render_template('communicate.html')
+
+
