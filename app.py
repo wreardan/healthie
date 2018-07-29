4de8370000811536c29a70c071855427f2aff8fb
Wesley Reardan <wesley@4kdev.com>
2018-07-28 21:13:18 -0700

    Fix failed lookup

--- app.py
+++ app.py
@@ -73,6 +73,8 @@ def login():
             cur = con.cursor()
             cur.execute("SELECT * FROM User WHERE email='%s'" % username)
             rows = cur.fetchall()
+            if len(rows) == 0
+                return render_template('login.html')
             user = rows[0]
             db_password_hash = user['password_hash']
             user_id = user['id']
