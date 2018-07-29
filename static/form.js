bc08aeed36e1c9011577ffe531529e2e0d872c81
Manavivian <h.vivian92@gmail.com>
2018-07-28 22:59:29 -0700

    Input color fields should only be applied to Register and Login files

---
+++ static/form.js
@@ -0,0 +1,22 @@
+/*
+This is the validation for the register file
+*/
+function validateRegister(form) {
+  let fail=validateFirst(form.firstname.value);
+}
+
+
+function validateLogin(form) {
+
+}
+
+
+function validateFirstName(field) {
+  if(field=="") return "Please enter your first name. \n"
+}
+
+function validateLastName(field) {
+
+}
+
+function validate
