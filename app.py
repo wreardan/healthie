8d4110f71b40eb8472e15894193becd091321d01
Wesley Reardan <wesley@4kdev.com>
2018-07-28 21:11:34 -0700

    Add login methods and login redirects if not logged in

--- app.py
+++ app.py
@@ -52,6 +52,9 @@ def register():
             cur.execute("INSERT INTO User(firstname, lastname, email, password_hash, phone, address, city, state, zipcode, reg_date) VALUES('%s', '%s', '%s', '%s', %d, '%s', '%s', '%s', %d, '%s');" % 
                 (firstname, lastname, email, password_hash, phone, address, city, state, zipcode, reg_date))
 
+            user_id = cur.lastrowid
+            session['user_id'] = uesr_id
+
         return redirect('/records')
 
     else:
@@ -70,7 +73,9 @@ def login():
             cur = con.cursor()
             cur.execute("SELECT * FROM User WHERE email=''")
             rows = cur.fetchall()
-            db_password_hash = rows[0]['password_hash']
+            user = rows[0]
+            db_password_hash = user['password_hash']
+            user_id = user['id']
 
         if user_id == '' or user_id is None:
             return render_template('login.html')
@@ -87,6 +92,8 @@ def login():
 
 @app.route('/records', methods = ['GET', 'POST'])
 def records():
+    if not 'user_id' in session or not (int(session['user_id']) > 0):
+        return redirect('/login')
     record_list = [
         {
             "filename": "Chest X-Ray",
@@ -109,11 +116,15 @@ def records():
 
 @app.route('/schedule', methods = ['GET', 'POST'])
 def schedule():
+    if not 'user_id' in session or not (int(session['user_id']) > 0):
+        return redirect('/login')
     return render_template('schedule.html')
 
 
 @app.route('/fitbit', methods = ['GET', 'POST'])
 def fitbit():
+    if not 'user_id' in session or not (int(session['user_id']) > 0):
+        return redirect('/login')
     return render_template('fitbit.html')
 
 
@@ -126,6 +137,8 @@ def fitbitredirect():
 
 @app.route('/communicate', methods = ['GET', 'POST'])
 def communicate():
+    if not 'user_id' in session or not (int(session['user_id']) > 0):
+        return redirect('/login')
     return render_template('communicate.html')
 
 
